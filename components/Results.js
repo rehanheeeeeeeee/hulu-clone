c5b8943dd5cce8ea4ed5a287b949c537c22f881c
rehanheeeeeeeee <rehantosif4@gmail.com>
2022-11-02 20:06:16 +0300

    committed

--- components/Results.js
+++ components/Results.js
@@ -4,7 +4,7 @@ import Thumbnail from "./Thumbnail";
 export default function Results({ movies }) {
   return (
     <div className="flex 2xl:grid-cols-4 justify-center flex-wrap my-10 px-5 items-center lg:grid-cols-3 sm:grid md:grid-cols-2 overflow-x-hidden">
-      {movies.map((movie, index) => (
+      {movies?.map((movie, index) => (
         <Thumbnail key={index} movie={movie} />
       ))}
     </div>
